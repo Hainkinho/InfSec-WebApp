68d21c0abb73ce3abc749781c3b36558fdaec68c
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2022-05-13 18:56:56 +0200

    Add DataMapper & Tests - to convert all posts relations into real objects

---
+++ dataMapper.js
@@ -0,0 +1,52 @@
+const User = require('./models/user')
+const Comment = require('./models/comment')
+
+module.exports = class DataMapper {
+    // static async mapPostRelationsToObj(post) {
+    //     let postRes = {}
+    //     const user = await User.findById(post.userID)
+    
+    //     postRes.id = post._id
+    //     postRes.author = user.name
+    //     postRes.text = post.text
+    
+    //     let comments = []
+    //     for (const i in post.comments) {
+    //         const commentID = post.comments[i]
+    //         const comment = await Comment.findById(commentID)
+    //         if (!comment) { continue }
+    //         const commentUser = await User.findById(comment.userID)
+    //         comments.push({
+    //             author: commentUser.name,
+    //             text: comment.text
+    //         })
+    //     }
+    
+    //     postRes.comments = comments
+    //     return postRes
+    // }
+
+    static async mapPostRelationsToObj(post) {
+        let postRes = post.toObject()
+        const user = await User.findById(post.userID)
+    
+        const userObj = user.toObject()
+        
+        postRes.user = userObj
+    
+        let comments = []
+        for (const i in post.comments) {
+            const commentID = post.comments[i]
+            const comment = await Comment.findById(commentID)
+            if (!comment) { continue }
+            const commentUser = await User.findById(comment.userID)
+            const commentUserObj = commentUser.toObject()
+            let commentObj = comment.toObject()
+            commentObj.user = commentUserObj
+            comments.push(commentObj)
+        }
+    
+        postRes.comments = comments
+        return postRes
+    }
+}
---
+++ tests/dataMapper.test.js
@@ -0,0 +1,39 @@
+const { MongoMemoryServer } = require('mongodb-memory-server')
+const { default: mongoose } = require('mongoose')
+const User = require('../models/user')
+const Post = require('../models/post')
+const Comment = require('../models/comment')
+const Repo = require('../repository')
+const DataMapper = require('../dataMapper')
+const InMemoryDB = require('./inMemoryDB')
+
+const inMemoryDB = new InMemoryDB()
+
+beforeAll(async () => {
+    return await inMemoryDB.connectDB()
+})
+
+beforeEach(async () => {
+    await inMemoryDB.clearDB()
+})
+
+afterAll(async () => { 
+    await inMemoryDB.closeDB()
+})
+
+
+
+test('test mapPostRelationsToObj', async () => {
+    const user = await Repo.createUser("a name", "test")
+    const post = await Repo.createPost(user, "A Post")
+    const comment = await Repo.createCommentForPost(post, user, "A Comment")
+
+    const postObj = await DataMapper.mapPostRelationsToObj(post)
+
+    expect(postObj.text).toEqual("A Post")
+    expect(postObj.user.name).toEqual(user.name)
+    expect(postObj.user.name).toEqual(user.name)
+    expect(postObj.comments.length).toEqual(1)
+    expect(postObj.comments[0].user.name).toEqual(user.name)
+    expect(postObj.comments[0].text).toEqual("A Comment")
+})
