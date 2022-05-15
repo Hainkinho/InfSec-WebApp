e5c59662cd927927e2c87c10df1c3105dd63f174
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2022-05-15 14:18:06 +0200

    Add design - add timecode for each post

--- dataMapper.js
+++ dataMapper.js
@@ -6,6 +6,8 @@ module.exports = class DataMapper {
         let postRes = post.toObject()
         const user = await User.findById(post.userID)
         postRes.user = user.toObject()
+        const formattedCreationDateString = DataMapper.formatTime(post.createdAt)
+        postRes.creationDateString = formattedCreationDateString
     
         let comments = []
         for (const i in post.comments) {
@@ -20,5 +22,31 @@ module.exports = class DataMapper {
     
         postRes.comments = comments
         return postRes
+    }
+
+    static formatTime(dateString) {
+        const date = new Date(dateString)
+        const hours = this.convertToTwoDigits(date.getHours()) 
+        const mins = this.convertToTwoDigits(date.getMinutes())
+
+        if (this.isToday(date)) {
+            return `Today - ${hours}:${mins}`
+        }
+
+        const day = date.getDay()
+        const month = date.getMonth()
+        const year = date.getFullYear()
+        return `${day}.${month}.${year} - ${hours}:${mins}`
+    }
+
+    static isToday(date) {
+        const now = new Date()
+        return date.getDay() == now.getDay() && 
+            date.getMonth() == now.getMonth() &&
+            date.getFullYear() == now.getFullYear()
+    }
+
+    static convertToTwoDigits(num) {
+        return num < 9 ? `0${num}` : num
     }
 }
