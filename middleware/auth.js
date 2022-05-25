71229fb92ae8b8d697cb547f88425e0046d80ab8
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2022-05-25 19:26:29 +0200

    Refactor - cleaner code

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,40 +4,52 @@ const User = require('../models/user')
 
 const userProtect = async function(req, res, next) {
     const token = req.cookies.token
-
-    if (!token) {
-        console.log("No Token provided in cookies")
+    const user = await getUserFromJWToken(token)
+    if (!user) {
         redirectToLoginPage(res)
         return
     }
+    req.user = user
+    next()
+}
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = await User.findById(decoded.id)
-        if (!req.user) {
-            console.log("No user found from jwtToken")
-            redirectToLoginPage(res)
-            return
-        }
-        next()
-    } catch (error) {
-        console.log("Couldn't verfiy token!")
+exports.adminOnlyProtect = async function(req, res, next) {
+    const token = req.cookies.token
+    const user = await getUserFromJWToken(token)
+    if (!user) {
         redirectToLoginPage(res)
+        return
     }
+    if (!user.isAdmin()) {
+        next(new CustomError(401, 'Admin only'))
+        return
+    }
+    req.user = user
+    next()
 }
 
 function redirectToLoginPage(res) {
     res.redirect('http://localhost:5000/login')
 }
 
-exports.adminOnlyProtect = async function(req, res, next) {
-    userProtect(req, res, () => {
-        if (!req.user.isAdmin()) {
-            next(new CustomError(401, 'Admin only'))
-            return
+async function getUserFromJWToken(token) {
+    if (!token) {
+        console.log("No Token provided in cookies")
+        return null
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded.id)
+        if (!user) {
+            console.log("No user found from jwtToken")
+            return null
         }
-        next()
-    })
+        return user
+    } catch (error) {
+        console.log("Couldn't verfiy token!")
+        return null
+    }
 }
 
 
