4c70c988f3da7ecfad675206e90dd4d1c68b7f29
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2022-05-25 15:30:43 +0200

    Add expireDate to csrvToken

--- CSRFValidator.js
+++ CSRFValidator.js
@@ -2,16 +2,24 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports =  class CSRFTokenValidator {
 
-    static _ids = []
+    static _ids = new Map()
 
     static generateID() {
         const id = uuidv4()
-        this._ids.push(id)
+        const expireDate = new Date(Date.now() + 10 * 60 * 1000)
+        this._ids.set(id, expireDate)
+        console.log(this._ids)
         return id
     }
 
     static isValid(id) {
         if (!id) { return false }
-        return this._ids.includes(id)
+        if (!this._ids.has(id)) { return false }
+        const expireDate = this._ids.get(id)
+        if (expireDate < new Date()) { 
+            this._ids.delete(id)
+            return false
+        }
+        return true
     }
 }
