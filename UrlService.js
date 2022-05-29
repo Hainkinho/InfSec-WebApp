ab9ebd3925f7a761ab594f0ee3c0fd4efe5619ff
Hainkinho <41513606+Hainkinho@users.noreply.github.com>
2022-05-29 09:46:03 +0200

    Minor Bug fix

--- UrlService.js
+++ UrlService.js
@@ -2,7 +2,7 @@
 const shouldSanitize = process.env.NODE_ENV == "sanitized"
 
  const getUrl = function(endpointString) {
-    const url = 'http://localhost:' + shouldSanitize ? '5050' : '5000'
+    const url = 'http://localhost:' + (shouldSanitize ? '5050' : '5000')
     return url + endpointString
 }
 
